3a62c725de082730aeb763b99d87d1f36837f8bc
alexrg17 <alexradugabriel7@gmail.com>
2025-02-12 14:19:10 +0000

    Added header component and suppressed Dart Sass warnings

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,10 @@
+import React from "react";
+import Header from "./components/Header";
+
 function App() {
   return (
     <div>
+      <Header /> {/* Add the Header component */}
       <h1>Game Website - Setup Complete</h1>
     </div>
   );
